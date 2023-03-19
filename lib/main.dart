f930f611d05dc43da18fd7491910ec3e88548b5e
iamudesharma <udesh2568@gmail.com>
2023-03-20 01:43:00 +0530

    responsive_builder web app icon added

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ResponsiveSizingConfig.instance.setCustomBreakpoints(
+    const ScreenBreakpoints(desktop: 800, tablet: 550, watch: 200),
+  );
   runApp(const MyApp());
 }
 
@@ -32,7 +38,7 @@ class MyApp extends StatelessWidget {
           DefaultCupertinoLocalizations.delegate,
         ],
         title: 'Flutter Platform Widgets',
-        home: HomePage(),
+        home: const HomePage(),
         material: (_, __) => MaterialAppData(
           theme: ThemeData(scaffoldBackgroundColor: Colors.black),
         ),
@@ -53,83 +59,126 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var deviceType = getDeviceType(MediaQuery.of(context).size);
     return Scaffold(
       backgroundColor: Colors.black,
-      body: DefaultTextStyle(
-        style: TextStyle(fontFamily: GoogleFonts.raleway().fontFamily),
-        child: Center(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                ),
-                ShaderMask(
-                  blendMode: BlendMode.srcATop,
-                  shaderCallback: (Rect bounds) {
-                    return LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      tileMode: TileMode.mirror,
-                      colors: [
-                        Colors.white,
-                        Colors.black26,
-                        // Color(0xffE1E1E1),
-                        // Colors.black26
-                      ],
-                    ).createShader(bounds);
-                  },
-                  child: Text("Build Digital",
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      )),
-                ),
-                ShaderMask(
-                  blendMode: BlendMode.srcATop,
-                  shaderCallback: (Rect bounds) {
-                    return LinearGradient(
-                      tileMode: TileMode.clamp,
-                      colors: [
-                        Colors.white,
-                        Colors.black26,
-                        Color(0xffE1E1E1),
-                        Colors.black26
-                      ],
-                    ).createShader(bounds);
-                  },
-                  child: Text("Products ,brands and",
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      )),
-                ),
-                ShaderMask(
-                  blendMode: BlendMode.srcATop,
-                  shaderCallback: (Rect bounds) {
-                    return LinearGradient(
-                      tileMode: TileMode.clamp,
-                      colors: [
-                        Colors.white,
-                        Colors.black26,
-                        Color(0xffE1E1E1),
-                        Colors.black26
-                      ],
-                    ).createShader(bounds);
-                  },
-                  child: Text("experiences",
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      )),
-                )
-              ]),
-        ),
-      ),
+      body: ResponsiveBuilder(builder: (context, size) {
+        return DefaultTextStyle(
+          style: TextStyle(fontFamily: GoogleFonts.raleway().fontFamily),
+          child: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: kToolbarHeight,
+                  ),
+                  if (!size.isMobile)
+                    SizedBox(
+                      width: size.screenSize.width * 0.8,
+                      height: 100,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                child: FlutterLogo(),
+                                backgroundColor: Colors.white12,
+                              ),
+                              Text(
+                                "Flutter Developer",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white70,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                "LinkedIn / Github / Instagram",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white70,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Divider(
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  const CircleAvatar(
+                    radius: 50,
+                  ),
+                  ShaderMask(
+                    blendMode: BlendMode.srcATop,
+                    shaderCallback: (Rect bounds) {
+                      return const LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        tileMode: TileMode.mirror,
+                        colors: [
+                          Colors.white,
+                          Colors.black26,
+                          // Color(0xffE1E1E1),
+                          // Colors.black26
+                        ],
+                      ).createShader(bounds);
+                    },
+                    child: const Text("Build Digital",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  ),
+                  ShaderMask(
+                    blendMode: BlendMode.srcATop,
+                    shaderCallback: (Rect bounds) {
+                      return const LinearGradient(
+                        tileMode: TileMode.clamp,
+                        colors: [
+                          Colors.white,
+                          Colors.black26,
+                          Color(0xffE1E1E1),
+                          Colors.black26
+                        ],
+                      ).createShader(bounds);
+                    },
+                    child: const Text("Products ,brands and",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  ),
+                  ShaderMask(
+                    blendMode: BlendMode.srcATop,
+                    shaderCallback: (Rect bounds) {
+                      return const LinearGradient(
+                        tileMode: TileMode.clamp,
+                        colors: [
+                          Colors.white,
+                          Colors.black26,
+                          Color(0xffE1E1E1),
+                          Colors.black26
+                        ],
+                      ).createShader(bounds);
+                    },
+                    child: const Text("experiences",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  )
+                ]),
+          ),
+        );
+      }),
     );
   }
 }
